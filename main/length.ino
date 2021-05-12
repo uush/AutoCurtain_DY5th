7a72226c371b3e0be4774d71829d9fdffff34932
PHYziiix <69707705+PHYziiix@users.noreply.github.com>
2021-05-12 22:46:11 +0900

    Update length.ino

--- main/length.ino
+++ main/length.ino
@@ -1,8 +1,10 @@
 #include <Arduino.h>
-void funcLength()
+#include "length.h"
+
+int funcLength()
 {
     if (bluetooth.available())
-        if (mode_meas==1)
+        // if "측정" 상황일 때
         {
             if ((extern currentCLK != extern lastCLK)  && extern currentCLK == 0)
              {
@@ -10,5 +12,6 @@ void funcLength()
              }
             y = (extern count>0)? extern count : (-1)* extern count;
             int length=(x+y)/2;
-        }    
+        }
+    return length    
 }
