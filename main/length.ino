908f860aa8b661074640f291fb199fbe092d2ff2
PHYziiix <69707705+PHYziiix@users.noreply.github.com>
2021-05-13 01:06:36 +0900

    Update length.ino

--- main/length.ino
+++ main/length.ino
@@ -3,15 +3,14 @@
 
 int funcLength()
 {
-    if (bluetooth.available())
-        // if "측정" 상황일 때
+    if (extern mode_meas==1)
+    {
+        if ((extern currentCLK != extern lastCLK)  && extern currentCLK == 0)
         {
-            if ((extern currentCLK != extern lastCLK)  && extern currentCLK == 0)
-             {
-                x ++;
-             }
-            y = (extern count>0)? extern count : (-1)* extern count;
-            int length=(x+y)/2;
+            x ++;
         }
-    return length    
+        y = (extern count>0)? extern count : (-1)* extern count;
+        int length=(x+y)/2;
+    }
+    return length;
 }
