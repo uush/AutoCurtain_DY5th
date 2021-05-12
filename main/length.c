47e501f7652f96022dfdfcc8adb0d43467a6619b
PHYziiix <69707705+PHYziiix@users.noreply.github.com>
2021-05-12 20:20:28 +0900

    Add files via upload

---
+++ main/length.c
@@ -0,0 +1,13 @@
+void funcLength()
+{
+    if (bluetooth.available())
+        // if "측정" 상황일 때
+        {
+            if ((currentCLK != lastCLK)  && currentCLK == 0)
+             {
+                x ++;
+             }
+            y = (extern count>0)? extern count : (-1)* extern count;
+            int length=(x+y)/2;
+        }    
+}
